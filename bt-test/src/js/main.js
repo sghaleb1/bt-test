25ded02a77987684e80609c2aa63837df579c51b
sghaleb1 <shahabghaleb@gmail.com>
2021-04-27 16:33:17 +0100

    final clean up

--- bt-test/src/js/main.js
+++ bt-test/src/js/main.js
@@ -1,71 +1,46 @@
 import '../css/main.css';
 import logo from '../logo.svg';
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
 
 function App() {
 
   const [newsData, setNewsData] = useState();
-
   const [searchData, setSearchData] = useState('BT');
   const [currentInput, setCurrentInput] = useState('');
-  //const [apiStatus, setApiStatus] = useState();
 
-  const searchedNews = useRef();
+  const pageSize = 10;
+  const dateFrom = '2021-04-26';
 
-  const apiKey = "3e7201adfdf84a0691f916e3488a31d8";//"64462cf79b3d449483ea5f2692e95e60";
-  const url = `https://newsapi.org/v2/everything?q=${searchData}&from=2021-04-26&pageSize=10&sortBy=popularity&apiKey=${apiKey}`
-
+  const apiKey = "3e7201adfdf84a0691f916e3488a31d8";
+  const url = `https://newsapi.org/v2/everything?q=${searchData}&from=${dateFrom}&pageSize=${pageSize}&sortBy=popularity&apiKey=${apiKey}`
   const req = new Request(url);
 
+  // ####################################################
+  // useEffect hook used to request NewsApi at beginning and when search input onChange.
+  // ####################################################
   useEffect(() => {
     fetch(req)
       .then((response) => response.json())
-      .then((data) => setNewsData(data)) //console.log(data)) 
+      .then((data) => setNewsData(data)) 
       .catch((error) => console.log(error));
   }, [searchData]);
-
-  useEffect(() => {
-    searchedNews.current = searchData
-  }, []);
-
+  
+  // ####################################################
+  // Only request newsApi only for correct input values, else error occurs.
+  // It was found that an error occurs when input is no value or 'space' 
+  // ####################################################
   function updateSearch(val) {
-
-    console.log(val.length, val)
     setCurrentInput(val)
-
-    // Prevent API error when input is no value or 'space' 
-    if (val.length > 0 && val != ' ') {
+    if (val.length > 0 && val !== ' ') {
       setSearchData(val)
     }
-
   }
 
-  //Dummy data due to API request limit reached
-  const test_title = "Forerunner’s Eurie Kim will share why she invested in Oura on Extra Crunch Live";
-  const test_data = `Oxygen and other supplies are unlikely to spare India further catastrophe. In Brazil, 
-                  Russia’s Sputnik V vaccine isn’t recommended. Greece eased its Oxygen and other supplies 
-                  are unlikely to spare India further catastrophe. In Brazil, Russia’s Sputnik V vaccine isn’t 
-                  recommended. Greece eased`;
-  const test_publisher = "New York Times";
-  const test_url = "https://blog.google/outreach-initiatives/diversity/a-djs-mission-to-tell-asian-american-stories-track-by-track/?utm_source=feedburner&utm_medium=feed&utm_campaign=Feed%3A+blogspot%2FMKuf+%28The+Keyword+%7C+Official+Google+Blog%29"
-
-  function TestNewsArticle() {
-    return (
-      <div className="news">
-        <h2 className="news_title">{test_title}</h2>
-        <span className="news_author">{test_publisher}</span>
-        <p className="news_desc">{test_data.replace(/(.{150})..+/, "$1…")}</p>
-        <span className="news_url">{test_url}</span>
-      </div>
-    );
-  }
-
-
-
+  // ####################################################
+  // News Article - Each article is the chosen layout and css 
+  // ####################################################
   function NewsArticle({ data }) {
-    //console.log("content length = ",data.description)
     return (
-
       <article className="news">
         <header>
           <h2 className="news-title">{data.title}</h2>
@@ -85,30 +60,29 @@ function App() {
       <header className="app-header">
         <div className="app-nav">
           <a href="https://www.bt.com/"><img src={logo} className="app-logo" alt="logo" /></a>
-
         </div>
-
       </header>
+
       <div className="container">
         <h1 className="container-header">BT React Code Test - by Shahab Ghaleb - 27/04/21</h1>
 
         <article className="all-news-container">
-        <div className="all-news">
-          <input className="search-input" value={currentInput} onChange={e => updateSearch(e.target.value)} placeholder="Search..." />
-          {newsData ? newsData.articles.map((newsItem) => (
-            <NewsArticle data={newsItem} key={newsItem.url} />
-          ))
-            : <div className="initial-loading">Loading</div>}
-            </div>
-        </article>
-        
+          <div className="all-news">
+            <input className="search-input" value={currentInput} onChange={e => updateSearch(e.target.value)} placeholder="Search..." />
+            {/* Display newsApi if response success, else return 'Loading...' */}
+            {newsData ? newsData.articles.map((newsItem) => (
+              <NewsArticle data={newsItem} key={newsItem.url} />
+            ))
+              : <div className="initial-loading">Loading...</div>}
 
+          </div>
+        </article>
       </div>
+
       <footer className="app-footer">
         <div className="footer-content">
           Footer - News article search
         </div>
-
       </footer>
     </div>
   );
